411b9f0ab4af7c48ddb32f0846352c096bbcafc2
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2023-10-10 17:44:43 +0530

    autoplay shut off

--- src/App.jsx
+++ src/App.jsx
@@ -34,9 +34,9 @@ const App = () => {
     slidesToScroll: 2,
     speed: 1500,
     swipeToSlide: true,
-    autoplay: true,
-    autoplaySpeed: 2500,
-    pauseOnHover: true,
+    // autoplay: true,
+    // autoplaySpeed: 2500,
+    // pauseOnHover: true,
     cssEase: "linear",
     responsive: [
       {
@@ -89,7 +89,7 @@ const App = () => {
           </div>
 
           {/* products */}
-          <div className="mx-auto mt-10 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none relative">
+          <div className="mx-auto mt-10 gap-x-8 gap-y-16 border-t border-gray-300 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none relative">
             <Slider
               ref={(slider) => {
                 sliderRef = slider;
